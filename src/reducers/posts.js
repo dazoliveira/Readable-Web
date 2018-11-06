bf19b8b0aad53b0363674002986292b177da9cfb
dazoliveira <douglaszanquetta@hotmail.com>
2018-11-06 01:18:18 -0200

    implements voting up

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,8 @@
 import { RECEIVE_POSTS } from '../actions/posts'
 import { SORT_POST_BY_DATE, SORT_POST_BY_SCORE, UPDATE_VOTE_SCORE } from '../actions/posts'
 
-export default function posts(state = {}, action) {
+
+export default function posts(state = [], action) {
     switch (action.type) {
         case RECEIVE_POSTS:
             return {
@@ -20,11 +21,8 @@ export default function posts(state = {}, action) {
             }
         case UPDATE_VOTE_SCORE:
             return {
-                ...state,
-                [0]: {
-                    ...state[0],
-                    voteScore: action.post.voteScore
-                }
+            ...state,
+            [Object.keys({...state}).filter(i => state[i].id === action.post.id)]: action.post
             }
         default:
             return state
